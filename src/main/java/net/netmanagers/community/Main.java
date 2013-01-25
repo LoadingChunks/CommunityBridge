50dd4d801d26b70a8d7fb00ea4575c809e2f803e
Ben Woodford <ben@tweedler.co.uk>
2013-01-25 13:18:27 +0000

    Catch null

--- src/main/java/net/netmanagers/community/Main.java
+++ src/main/java/net/netmanagers/community/Main.java
@@ -753,11 +753,11 @@ public class Main extends JavaPlugin {
 							// Cache the strings, we're optimising removals here.
 							ArrayList<String> theirGroups = new ArrayList<String>();
 							for (String g : sortableGroups) {
-								if(prerequisites.isSet(
+								if(prerequisites != null && prerequisites.isSet(
 										groups.get(g).toString())
-											&& !extra_groups.contains(
+											&& (extra_groups == null || !extra_groups.contains(
 												String.valueOf(
-														prerequisites.getInt((String)groups.get(g)))))
+														prerequisites.getInt((String)groups.get(g))))))
 									continue;
 								
 								if(groups.containsKey(g))
